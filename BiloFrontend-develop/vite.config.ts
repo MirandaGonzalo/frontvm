d9c123e450732931db38d4c25f1afc17ffd18826
Miranda Gonzalo <gonzamirandab2000@gmail.com>
2025-06-29 23:35:09 -0300

    ......

--- BiloFrontend-develop/vite.config.ts
+++ BiloFrontend-develop/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: ['bilodamiani.com.ar'],
-  },
+  plugins: [react(), tsconfigPaths()],
 })
